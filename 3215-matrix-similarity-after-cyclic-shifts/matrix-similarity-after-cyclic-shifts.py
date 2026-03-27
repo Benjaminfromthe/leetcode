d89659ab85a274fb6e7e443fdd12068deb03a3f6
Benjamin Nshimiye <benjaminnshimiye633@gmail.com>
2026-03-27 14:33:37 +0200

    Time: 4 ms (37.65%) | Memory: 19.4 MB (66.47%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
@@ -1,12 +1,11 @@
 class Solution:
-    def areSimilar(self, mat: list[list[int]], k: int) -> bool:
-        n = len(mat[0])
-        # We only need to check if each row is 'k-periodic'
-        # Because shifting left/right by k is functionally the same 
-        # check for equality in a cyclic array.
-        for row in mat:
-            for j in range(n):
-                # Check the element k positions away (cyclic)
-                if row[j] != row[(j + k) % n]:
+    def areSimilar(self, mat: List[List[int]], k: int) -> bool:
+        n= len(mat[0])
+
+        for r in range(len(mat)):
+            for i in range(n):
+                if mat[r][i] != mat[r][(i+k)%n]:
                     return False
         return True
+
+        
